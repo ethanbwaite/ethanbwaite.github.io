ec333b3203c76d0404f026a759535779bba3a7bd
ebwaite <ethanbekwaite@gmail.com>
2020-01-19 15:25:51 -0800

    Big polish update

--- canvasScript.js
+++ canvasScript.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
-ctx.canvas.width = window.innerWidth - 100;
-ctx.canvas.height = window.innerHeight - 100;
+ctx.canvas.width = 800;//window.innerWidth - 100;
+ctx.canvas.height = 600;//window.innerHeight - 300;
 
 const cellX = 25;
 const cellY = 25;
@@ -12,30 +12,66 @@ ctx.canvas.height -= ctx.canvas.height % cellY;
 const numXCells = Math.floor(ctx.canvas.width / cellX) - 1;
 const numYCells = Math.floor(ctx.canvas.height / cellY) - 1;
 
+
+var drawNom = false;
+var nomX = 100;
+var nomY = 100;
+var nomAlpha = 1;
+var nomDelta = 1;
+
 var score = 0;
 
-var foodX = 500;
-var foodY = 500;
+var interval;
+
+var foodX = 0;
+var foodY = 0;
 
 
 var direction = 1;
+var snekMoved = true;
+var snekStep = 10;//number of intervals to wait before moving
+var currentStep = 0;
 
 function Seg(x, y) {
     this.xPos = x;
     this.yPos = y;
 }
 
-var startColor = [247,10,196];
-var endColor = [247,208,10];
+var startColor = [10,178,240];
+var endColor = [242,33,61];
+var foodColor = [255,255,255];
 
 
 
 var segments = [new Seg(Math.floor(numXCells/2)*cellX, Math.floor(numYCells/2)*cellY), new Seg(Math.floor(numXCells/2)*cellX, (Math.floor(numYCells/2) + 1)*cellY)];
 
+//disable scrollbars
+document.documentElement.style.overflow = 'hidden';  
 
-//for (i=1;i<3;i++){
-  //  segments.push(new Seg(Math.floor(numXCells/2)*cellX, (Math.floor(numYCells/2) + 1)*cellY));
-//}
+
+/*for (i=1;i<400;i++){
+    segments.push(new Seg(Math.floor(numXCells/2)*cellX, (Math.floor(numYCells/2) + 1)*cellY));
+}*/
+
+function animateNom(){
+    if (drawNom = true){
+        ctx.fillStyle = "rgba(255,255,255,"+nomAlpha+")";
+        ctx.font = "12px 'Press Start 2P'";
+        ctx.fillText("NOM", nomX, nomY);
+        nomY -= nomDelta;
+        nomAlpha -= 0.01;
+        if (nomDelta > 0){
+            nomDelta -= 0.01;
+        } else {
+            drawNom = false;
+        }
+    }
+}
+
+function resetNom(){
+    nomAlpha = 1;
+    nomDelta = 1;
+}
 
 function drawSquare(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
@@ -50,102 +86,117 @@ function drawSquare(){
     }
     
     //Draw Food
-    ctx.fillStyle = 'white';
+    ctx.fillStyle = 'rgb(' + foodColor[0] + ', ' + foodColor[1] + ', ' + foodColor[2] + ')';
     ctx.fillRect(foodX, foodY, cellX, cellY);
+}
+
+function startClicked(){
+    interval = setInterval(snek, 10);
+    document.getElementById("buttonStart").style.display = "none";
+    document.getElementById("gameOver").textContent = ""
+    segments = [new Seg(Math.floor(numXCells/2)*cellX, Math.floor(numYCells/2)*cellY), new Seg(Math.floor(numXCells/2)*cellX, (Math.floor(numYCells/2) + 1)*cellY)];
+    placeFood();
+    setScore(0);
 }
 
 //snek step
 function snek(){
-    
-    shiftSegs(segments);
-    switch (direction){
-        case 0:
-            //alert('Left Pressed');
-            segments[0].xPos -= cellX;
-            break;
-        case 1:
-            //alert('Right Pressed');
-            segments[0].xPos += cellX;
-            break;
-        case 2:
-            //alert('Up Pressed');
-            segments[0].yPos -= cellY;
-            break;
-        case 3:
-            //alert('Down Pressed');
-            segments[0].yPos += cellY;
-            break;
-    }
-    
-    checkFood();
-    checkSnek();
-    
-    
-    
+    currentStep++;
     drawSquare();
+    animateNom();
+    if (currentStep > snekStep){
+        currentStep = 0;
+        checkFood();
+
+        shiftSegs(segments);
+        switch (direction){
+            case 0:
+                //alert('Left Pressed');
+                segments[0].xPos -= cellX;
+                break;
+            case 1:
+                //alert('Right Pressed');
+                segments[0].xPos += cellX;
+                break;
+            case 2:
+                //alert('Up Pressed');
+                segments[0].yPos -= cellY;
+                break;
+            case 3:
+                //alert('Down Pressed');
+                segments[0].yPos += cellY;
+                break;
+        }
+        checkSnek();
+        
+        snekMoved = true;
+    }
 }
 
 //Handle keyboard input
 document.onkeydown = function(event) {
-    switch (event.keyCode){
-        //Arrow Keys
-        case 37:
-            //alert('Left Pressed');
-            if (direction != 1){
-                direction = 0;
-            }
-            break;
-        case 39:
-            //alert('Right Pressed');
-            if (direction != 0){
-                direction = 1;
-            }
-            break;
-        case 38:
-            //alert('Up Pressed');
-            if (direction != 3){
-                direction = 2;
-            }
-            break;
-        case 40:
-            //alert('Down Pressed');
-            if (direction != 2){
-                direction = 3;
-            }
-            break;
-        
-        //WASD    
-        case 65:
-            //alert('Left Pressed');
-            if (direction != 1){
-                direction = 0;
-            }
-            break;
-        case 68:
-            //alert('Right Pressed');
-            if (direction != 0){
-                direction = 1;
-            }
-            break;
-        case 87:
-            //alert('Up Pressed');
-            if (direction != 3){
-                direction = 2;
-            }
-            break;
-        case 83:
-            //alert('Down Pressed');
-            if (direction != 2){
-                direction = 3;
-            }
-            break;    
-        
-        //Spacebar
-        case 32:
-            setInterval(snek, 75);
-            break;
+    if (snekMoved){
+        switch (event.keyCode){
+            //Arrow Keys
+            case 37:
+                //alert('Left Pressed');
+                if (direction != 1){
+                    direction = 0;
+                }
+                break;
+            case 39:
+                //alert('Right Pressed');
+                if (direction != 0){
+                    direction = 1;
+                }
+                break;
+            case 38:
+                //alert('Up Pressed');
+                if (direction != 3){
+                    direction = 2;
+                }
+                break;
+            case 40:
+                //alert('Down Pressed');
+                if (direction != 2){
+                    direction = 3;
+                }
+                break;
+
+            //WASD    
+            case 65:
+                //alert('Left Pressed');
+                if (direction != 1){
+                    direction = 0;
+                }
+                break;
+            case 68:
+                //alert('Right Pressed');
+                if (direction != 0){
+                    direction = 1;
+                }
+                break;
+            case 87:
+                //alert('Up Pressed');
+                if (direction != 3){
+                    direction = 2;
+                }
+                break;
+            case 83:
+                //alert('Down Pressed');
+                if (direction != 2){
+                    direction = 3;
+                }
+                break;    
+
+            //Spacebar
+            case 32:
+                //setInterval(snek, 100);
+                break;
+        }
     }
     event.preventDefault();
+    snekMoved = false;
 }
 
 function shiftSegs(s){
@@ -155,30 +206,26 @@ function shiftSegs(s){
     }
 }
 
+//create new food after eating
 function placeFood(){
-    var xCheck = true;
-    var yCheck = true;
+    foodX = Math.floor(Math.random() * (numXCells-2)) * cellX;
+    foodY = Math.floor(Math.random() * (numYCells-2)) * cellY;
     
-    while(xCheck) {
-        xCheck = false;
-        foodX = Math.floor(Math.random() * (numXCells-1)) * cellX;
-        for (i=0; i < segments.length; i++){
-            if (segments[i].xPos == foodX)
-                xCheck = true;
-                //alert("food conflict x");
+    for (i=0;i<segments.length;i++){
+        if (segments[i].xPos == foodX && segments[i].yPos == foodY){
+            foodX = Math.floor(Math.random() * (numXCells-2)) * cellX;
+            foodY = Math.floor(Math.random() * (numYCells-2)) * cellY;
+            
+            i=0;
         }
     }
     
-    while(yCheck) {
-        yCheck = false;
-        foodY = Math.floor(Math.random() * (numYCells-1)) * cellY;
-        for (i=0; i < segments.length; i++){
-            if (segments[i].yPos == foodY)
-                yCheck = true;
-                //alert("food conflict y")
-        }
-    }
+    console.log("Food placed: " + foodX + ", " + foodY);
     
+    /*foodColor[0] = Math.floor(Math.random()*255);
+    foodColor[1] = Math.floor(Math.random()*255);
+    foodColor[2] = Math.floor(Math.random()*255);*/
+
 }
 
 //Check failure conditions
@@ -187,35 +234,54 @@ function checkSnek(){
     for (i=1; i < segments.length; i++){
         if (segments[0].xPos == segments[i].xPos && segments[0].yPos == segments[i].yPos){
             //game end event
-            alert("Game Over: Snek");
+            //alert("Game Over: Snek");
             endGame();
         }
     }
     
     if (segments[0].xPos < 0 || segments[0].xPos > (numXCells * cellX) || segments[0].yPos < 0 || segments[0].yPos > (numYCells * cellY)) {
-        alert("Game Over: Wall");
+        //alert("Game Over: Wall");
         endGame();
     }
 }
 
+//snek eat food?
 function checkFood(){
     if (segments[0].xPos == foodX && segments[0].yPos == foodY){
-        segments.push(new Seg(segments[segments.length-1].xPos,segments[segments.length-1].yPos));
+        for (i=0;i<5;i++){
+            segments.push(new Seg(segments[segments.length-1].xPos,segments[segments.length-1].yPos));
+        }
+        
+        /*for(i=0;i<3;i++){
+            endColor[i] = startColor[i];
+        }*/
+        /*for(i=0;i<3;i++){
+            startColor[i] = foodColor[i];
+        }*/
+        resetNom();
+        nomX = foodX;
+        nomY = foodY;
+        drawNom = true;
+        
         placeFood();
         setScore(score+1);
+        
+        
     }
 }
 
 function endGame() {
     //End game event, reset snek
-    segments = [new Seg(Math.floor(numXCells/2)*cellX, Math.floor(numYCells/2)*cellY), new Seg(Math.floor(numXCells/2)*cellX, (Math.floor(numYCells/2) + 1)*cellY)];
-    placeFood();
-    setScore(0);
+    
+    clearInterval(interval);
+    document.getElementById("buttonStart").style.display = "inline-block";
+    document.getElementById("gameOver").textContent = "DED SNEK"
+
 }
 
 function setScore(newScore){
     score = newScore;
-    document.getElementById("score").textContent = "snek: " + score;
+    document.getElementById("score").textContent = "SNEK: " + score + "\n";
 }
 
 function interpolateRGB(start,end,step,totalSteps){
